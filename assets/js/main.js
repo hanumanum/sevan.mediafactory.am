64f29218a09a20bf49d776803b3767987f1fe336
hanuman <a.kolyan@gratun.am>
2021-05-24 15:27:33 +0400

    manu and other pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,4 +7,29 @@ $(document).ready(function () {
   };
   click("#menu", "0px", "-1", "0");
   click("#cancel", "-300px", "1", "1");
+  makeNavigation()
+
+
+  function makeNavigation() {
+    $('#main-menu').load('./templates/nav.html', function () {
+      $('.menu-item').removeClass("menu-item-active")
+      const slugs = window.location.pathname.split("/")
+      const slug = slugs[slugs.length-1]
+      const slug_article = slugs[slugs.length-2]
+     
+      $('.menu-item a').each(function (i, l) {
+        if(slug_article == 'articles' && $(l).prop("href").indexOf("articles")){
+          $(l).parent().addClass("menu-item-active")
+        }
+        
+        if($(l).prop("href")!="" &&  $(l).prop("href").indexOf(slug)>=0){
+          $(l).parent().addClass("menu-item-active")
+        }
+      })
+    });
+
+  }
+
+
 });
+
