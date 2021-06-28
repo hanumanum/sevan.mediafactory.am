e7b6cbee294b84b23ca7a88ae57b9f7253b05ecf
hanuman <a.kolyan@gratun.am>
2021-06-28 15:09:43 +0400

    mobile improvements

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,27 @@
 $(document).ready(function () {
   jQuery.migrateMute = true;
 
-  const click = (tag, pixel, zindex, transition) => {
-    $(tag).on("click", () => {
+  function click(tag, pixel, zindex, transition) {
+    console.log(tag, pixel, zindex, transition)
+    $(tag).on("click", function (el) {
+      console.log(el)
       $("#main-menu").css({ "margin-right": pixel });
       $(".elem4").css({ "z-index": zindex, transition: `${transition}s` });
+      click("#cancel", "-300px", "1", "1");
     });
   };
   click("#menu", "0px", "-1", "0");
-  click("#cancel", "-300px", "1", "1");
+  
   makeNavigation()
   makeFooter()
 
-  stopPageScroll()
+  //stopPageScroll()
 
   function makeFooter() {
     $("footer").load("./templates/footer.html")
   }
 
+  
   function makeNavigation() {
     $('#main-menu').load('./templates/nav.html', function () {
       $('.menu-item').removeClass("menu-item-active")
@@ -35,10 +39,6 @@ $(document).ready(function () {
         }
       })
     });
-
-
-
   }
-
 });
 
